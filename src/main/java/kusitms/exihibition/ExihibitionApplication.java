3bd0378e99814c45145fa4af93bc1799db5e0bf2
bbbang105 <2018111366@dgu.ac.kr>
2024-11-08 18:58:24 +0900

    [init] : Initial Commit

---
+++ src/main/java/kusitms/exihibition/ExihibitionApplication.java
@@ -0,0 +1,13 @@
+package kusitms.exihibition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExihibitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExihibitionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kusitms/exihibition/ExihibitionApplicationTests.java
@@ -0,0 +1,13 @@
+package kusitms.exihibition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExihibitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
