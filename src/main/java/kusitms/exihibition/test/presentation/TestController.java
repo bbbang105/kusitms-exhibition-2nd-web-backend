710bc2ee4e4828dc5e7ce1e7f4c83bfa58439cff
bbbang105 <2018111366@dgu.ac.kr>
2024-11-15 22:16:19 +0900

    #7 [feat] : 헬스체크용 API를 추가한다

---
+++ src/main/java/kusitms/exihibition/test/presentation/TestController.java
@@ -0,0 +1,21 @@
+package kusitms.exihibition.test.presentation;
+
+import kusitms.exihibition.global.dto.ApiResponse;
+import kusitms.exihibition.global.status.SuccessStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/test")
+public class TestController {
+
+    @GetMapping("/health-check")
+    public ResponseEntity<ApiResponse<Object>> healthCheck(){
+
+        return ApiResponse.onSuccess(SuccessStatus._OK, "Good!");
+    }
+}
