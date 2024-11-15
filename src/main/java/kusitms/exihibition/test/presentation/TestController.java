b2b84a03e74118e3efd5a86133834148b81fc2b0
bbbang105 <2018111366@dgu.ac.kr>
2024-11-15 22:25:57 +0900

    #8 [feat] : 에러 테스트 API를 추가한다

--- src/main/java/kusitms/exihibition/test/presentation/TestController.java
+++ src/main/java/kusitms/exihibition/test/presentation/TestController.java
@@ -1,6 +1,8 @@
 package kusitms.exihibition.test.presentation;
 
 import kusitms.exihibition.global.dto.ApiResponse;
+import kusitms.exihibition.global.exception.CustomException;
+import kusitms.exihibition.global.status.ErrorStatus;
 import kusitms.exihibition.global.status.SuccessStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,5 +19,10 @@ public class TestController {
     public ResponseEntity<ApiResponse<Object>> healthCheck(){
 
         return ApiResponse.onSuccess(SuccessStatus._OK, "Good!");
+    }
+
+    @GetMapping("/error")
+    public void error(){
+        throw new CustomException(ErrorStatus._BAD_REQUEST);
     }
 }
