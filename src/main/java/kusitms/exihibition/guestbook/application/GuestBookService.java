999d6fe01a98084846678746cb001efde6ad69d1
bbbang105 <2018111366@dgu.ac.kr>
2024-11-16 00:31:41 +0900

    #13 [feat] : 페이지 별로 방명록을 조회할 수 있다

--- src/main/java/kusitms/exihibition/guestbook/application/GuestBookService.java
+++ src/main/java/kusitms/exihibition/guestbook/application/GuestBookService.java
@@ -3,10 +3,16 @@ package kusitms.exihibition.guestbook.application;
 import kusitms.exihibition.guestbook.domain.entity.GuestBook;
 import kusitms.exihibition.guestbook.domain.repository.GuestBookRepository;
 import kusitms.exihibition.guestbook.dto.request.RegisterGuestBookRequest;
+import kusitms.exihibition.guestbook.dto.response.GetGuestBooksResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -15,9 +21,20 @@ public class GuestBookService {
 
     private final GuestBookRepository guestBookRepository;
 
+    // 방명록 등록 메서드
     @Transactional
     public void registerGuestBook(RegisterGuestBookRequest request) {
         GuestBook newGuestBook = request.toEntity();
         guestBookRepository.save(newGuestBook);
+    }
+
+    // 방명록 조회 메서드 (1페이지당 10개)
+    @Transactional(readOnly = true)
+    public List<GetGuestBooksResponse> getGuestBooks(Pageable pageable) {
+        Page<GuestBook> guestBooksPage = guestBookRepository.findAllByOrderByCreatedDateDesc(pageable);
+
+        return guestBooksPage.stream()
+                .map(GetGuestBooksResponse::from)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/kusitms/exihibition/guestbook/presentation/GuestBookController.java
+++ src/main/java/kusitms/exihibition/guestbook/presentation/GuestBookController.java
@@ -1,17 +1,19 @@
 package kusitms.exihibition.guestbook.presentation;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import kusitms.exihibition.global.dto.ApiResponse;
 import kusitms.exihibition.guestbook.application.GuestBookService;
 import kusitms.exihibition.guestbook.dto.request.RegisterGuestBookRequest;
+import kusitms.exihibition.guestbook.dto.response.GetGuestBooksResponse;
 import kusitms.exihibition.guestbook.status.GuestBookSuccessStatus;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -36,5 +38,22 @@ public class GuestBookController {
 
         guestBookService.registerGuestBook(request);
         return ApiResponse.onSuccess(GuestBookSuccessStatus._REGISTER_GUESTBOOK);
+    }
+
+    /**
+     * 방명록 목록 조회 API
+     *
+     * 최신순으로 정렬된 방명록 데이터를 반환합니다.
+     *
+     * @param page 조회할 페이지 번호 (1부터 시작)
+     * @return 방명록 데이터 목록 (1페이지당 10개)
+     */
+    @GetMapping("/{page}")
+    public ResponseEntity<ApiResponse<List<GetGuestBooksResponse>>> getGuestBooks(
+            @PathVariable("page") @Min(1) int page
+    ) {
+
+        List<GetGuestBooksResponse> guestBooks = guestBookService.getGuestBooks(PageRequest.of(page - 1, 10));
+        return ApiResponse.onSuccess(GuestBookSuccessStatus._GET_GUESTBOOKS_BY_PAGE, guestBooks);
     }
 }
