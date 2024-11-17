c355ebaa887dea287d0036e6dec7c9aad4bc24ce
bbbang105 <2018111366@dgu.ac.kr>
2024-11-17 14:44:55 +0900

    #26 [feat] : 방명록의 길이를 200자로 제한한다

--- src/main/java/kusitms/exihibition/guestbook/dto/request/RegisterGuestBookRequest.java
+++ src/main/java/kusitms/exihibition/guestbook/dto/request/RegisterGuestBookRequest.java
@@ -1,10 +1,13 @@
 package kusitms.exihibition.guestbook.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import kusitms.exihibition.guestbook.domain.entity.GuestBook;
 
 public record RegisterGuestBookRequest(
-        @NotBlank(message = "방명록 내용은 빈 값일 수 없습니다.") String content
+        @NotBlank(message = "방명록 내용은 빈 값일 수 없습니다.")
+        @Size(max = 200, message = "방명록 내용은 200자를 초과할 수 없습니다.")
+        String content
 ) {
 
     public GuestBook toEntity() {
