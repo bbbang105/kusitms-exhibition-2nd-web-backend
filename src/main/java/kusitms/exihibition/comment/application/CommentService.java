715bc9c0f1166fbc5f06000925b814f3080bb6d1
bbbang105 <2018111366@dgu.ac.kr>
2024-11-18 01:29:07 +0900

    #26 [feat] : 페이지 별로 의견을 조회할 수 있다

--- src/main/java/kusitms/exihibition/comment/application/CommentService.java
+++ src/main/java/kusitms/exihibition/comment/application/CommentService.java
@@ -1,6 +1,7 @@
 package kusitms.exihibition.comment.application;
 
 import kusitms.exihibition.comment.dto.request.RegisterCommentRequest;
+import kusitms.exihibition.comment.dto.response.GetCommentsResponse;
 import kusitms.exihibition.comment.entity.Comment;
 import kusitms.exihibition.comment.entity.repository.CommentRepository;
 import kusitms.exihibition.global.exception.CustomException;
@@ -9,8 +10,12 @@ import kusitms.exihibition.product.domain.repository.ProductRepository;
 import kusitms.exihibition.product.status.ProductErrorStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -27,5 +32,22 @@ public class CommentService {
                 .orElseThrow(() -> new CustomException(ProductErrorStatus._NOT_FOUND_PRODUCT));
         Comment newComment = request.toEntity(product);
         commentRepository.save(newComment);
+    }
+
+    // 의견 조회 메서드 (1페이지당 10개)
+    @Transactional(readOnly = true)
+    public GetCommentsResponse getComments(Long productId, Pageable pageable) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new CustomException(ProductErrorStatus._NOT_FOUND_PRODUCT));
+        Page<Comment> commentsPage = commentRepository.findByProductOrderByCreatedDateDesc(product, pageable);
+
+        List<GetCommentsResponse.CommentResponse> comments = commentsPage.stream()
+                .map(GetCommentsResponse.CommentResponse::from)
+                .toList();
+
+        int totalGuestBookCount = (int) commentsPage.getTotalElements();
+        int totalPageCount = commentsPage.getTotalPages();
+
+        return GetCommentsResponse.of(totalGuestBookCount, totalPageCount, comments);
     }
 }
--- src/main/java/kusitms/exihibition/comment/entity/repository/CommentRepository.java
+++ src/main/java/kusitms/exihibition/comment/entity/repository/CommentRepository.java
@@ -1,7 +1,11 @@
 package kusitms.exihibition.comment.entity.repository;
 
 import kusitms.exihibition.comment.entity.Comment;
+import kusitms.exihibition.product.domain.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Page<Comment> findByProductOrderByCreatedDateDesc(Product product, Pageable pageable);
 }
